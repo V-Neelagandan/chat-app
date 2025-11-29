46fba99f257a2c72c5aa437c7f9d0782e0afa5c9
Your NameV-Neela <neelagandan6568@gmail.com>
2025-11-29 13:09:15 +0530

    Switch database to SQLite for Render

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -110,7 +110,7 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME"),
@@ -122,8 +122,14 @@ DATABASES = {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
-}
+}"""
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
